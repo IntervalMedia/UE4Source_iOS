d1b01892b9afa0b0285986ac4b6d128aba76432f
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2025-03-23 23:14:23 +1000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,7 @@
 #include "Memory.h"
 #include "Cheats.h"
 
-static void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info)
-{
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-       main();
-    });
-}
-
-__attribute__((constructor)) static void initialize()
-{
-    CFNotificationCenterAddObserver(CFNotificationCenterGetLocalCenter(), NULL, &didFinishLaunching, (CFStringRef)UIApplicationDidFinishLaunchingNotification, NULL, CFNotificationSuspensionBehaviorDrop);
-}
-
-
-int main()
+void mainios()
 {
 	
 long ModuleBaseAddress = (long)_dyld_get_image_header(0);
@@ -24,3 +11,16 @@ long ModuleBaseAddress = (long)_dyld_get_image_header(0);
 	}
 
 }
+
+static void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info)
+{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+       mainios();
+    });
+}
+
+__attribute__((constructor)) static void initialize()
+{
+    CFNotificationCenterAddObserver(CFNotificationCenterGetLocalCenter(), NULL, &didFinishLaunching, (CFStringRef)UIApplicationDidFinishLaunchingNotification, NULL, CFNotificationSuspensionBehaviorDrop);
+}
+
