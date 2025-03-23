9b44a4236c3a23b65897eec60937cbbba262630d
silentninjabee <91723045+silentninjabee@users.noreply.github.com>
2025-03-23 23:12:36 +1000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,7 @@
 #include "Memory.h"
-#include "Cheats.static 
+#include "Cheats.h"
 
-void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info)
+static void didFinishLaunching(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef info)
 {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
        main();
@@ -16,16 +16,11 @@ __attribute__((constructor)) static void initialize()
 
 int main()
 {
-	// ONCE AGAIN THIS IS WILL GET YOU BANNED ON BATTLEYE SERVERS, ADD IN YOUR OWN DRIVER TO RPM
-	HWND window = FindWindowA(NULL, "ArkAscended");
-	GetWindowThreadProcessId(window, &Memory::ProcessID);
-	if (Memory::ProcessID == 0) return 0;
-
-	Memory::ProcessHandle = OpenProcess(PROCESS_ALL_ACCESS, false, Memory::ProcessID);
-	Memory::ModuleBaseAddress = FindModuleBaseAddress(Memory::ProcessID, "ArkAscended.exe");
 	
-	if (Memory::ModuleBaseAddress != 0)
+long ModuleBaseAddress = (long)_dyld_get_image_header(0);
+	
+	if (ModuleBaseAddress != 0){
 		Cheats::Run();
-	else
-		return 0;
+	}
+
 }
